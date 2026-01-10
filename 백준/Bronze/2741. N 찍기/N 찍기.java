3211b2618a2cf446f2c03023fe95ee4778cca293
jin605 <jinddd3@gmail.com>
2026-01-10 16:47:42 +0900

    [Bronze V] Title: N 찍기, Time: 836 ms, Memory: 34064 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int a =1; N>a ; a++)
+            System.out.println(a);
+        System.out.println(N);
+
+
+
+
+    }
+
+}
